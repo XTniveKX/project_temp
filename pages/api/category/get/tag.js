08524ae4595d3001864e832d98f73cbd0534acb0
Kevin Tabaraka <66250033+XTniveKX@users.noreply.github.com>
2024-02-02 17:20:19 +0700

    Add files via upload

--- pages/api/category/get/tag.js
+++ pages/api/category/get/tag.js
@@ -10,7 +10,6 @@ const pool = new Pool({
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    console.log("ASDSAODKSAODKASDK")
     try {
       const { tag } = req.query;
       const client = await pool.connect();
